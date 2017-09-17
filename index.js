079951d812f89ffcd11e7baa60a401b3c2ec9704
user <user@lab1562.tumo.lab>
2017-09-17 11:33:30 +0400

    add package.json

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-console.log("HelloSparta");
-console.log("this is it");
+var scrapeIt = require("scrape-it");
+var jsonfile = require('jsonfile');
+
+var file = 'data.json';
+
+scrapeIt("http://ionicabizau.net", {
+    title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+    
+}).then(function(page)
+{
+    jsonfile.writeFile(file, page, {spaces: 2}, function(err)
+    {
+        console.error(err);
+    })
+});
+scrapeIt("http://ionicabizau.net", {
+    articles: {
+        listItem: ".article"
+      , data: {
+            createdAt: {
+                selector: ".date"
+              , convert: x => new Date(x)
+            }
+          , title: "a.article-title"
+          , tags: {
+                listItem: ".tags > span"
+            }
+          , content: {
+                selector: ".article-content"
+              , how: "html"
+            }
+        }
+    }
+  , pages: {
+        listItem: "li.page"
+      , name: "pages"
+      , data: {
+            title: "a"
+          , url: {
+                selector: "a"
+              , attr: "href"
+            }
+        }
+    }
+  , title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}, (err, page) => {
+    jsonfile.writeFile(file, page, { flag: 'a', spaces: 2}, function(err)
+    {
+        console.error(err);
+    })
+});
