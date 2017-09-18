32249a2d015d5f4d6885f33450a032fbd3f8c006
user <user@lab1554.tumo.lab>
2017-09-18 18:51:58 +0400

    changed scraper.js

--- scraper.js
+++ scraper.js
@@ -4,11 +4,19 @@ var jsonfile = require('jsonfile');
 var file = 'data.json';
 
 scrapeIt("http://list.am/category/62", {
-    prices: ".l2 .l"
-  , homes: ".h .l"
+    houses: {
+        listItem: "#contentr > div.dl > div.gl > a",
+        data: {
+            title: "div:nth-child(2)",
+            url: {
+                closest: "a",
+                attr: "href"
+            }
+        }
+    }
 }).then(function(page)
 {
-    jsonfile.writeFile(file, page, {flag:'a', spaces: 2}, function(err)
+    jsonfile.writeFile(file, page, {spaces: 2}, function(err)
     {
         console.error(err);
     })
