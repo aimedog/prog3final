a0c659cf7c8770eae3a1e283be51ebad76a056ff
user <user@lab1560.tumo.lab>
2017-09-29 19:20:43 +0400

    adjust scraper

--- scraper.js
+++ scraper.js
@@ -2,10 +2,12 @@ var scrapeIt = require("scrape-it");
 var jsonfile = require('jsonfile');
 var fs = require("fs");
 
-var file = 'data.json'
+var file = 'data.json';
 
 var datax = { "houses": [] };
-var urlfile = 'urlfile.json'
+var urlfile = 'urlfile.json';
+var datay = { "houses": [] };
+var url_data;
 
 function scraper_list(page_number) {
     scrapeIt("http://list.am/category/62/" + page_number, {
@@ -21,14 +23,15 @@ function scraper_list(page_number) {
         }
     }).then(function (page) {
         datax.houses.push(...page.houses);
-        console.log('datax: ' + datax.houses.length);
         if (page_number == 2) {
-            jsonfile.writeFile(urlfile, datax, { spaces: 2 }, scraper_houses);
+            jsonfile.writeFile(urlfile, datax, { spaces: 2 }, function () {
+                // starting scraper for houses
+                start()
+            });
         } else {
             setTimeout(
                 () => {
                     page_number++;
-                    console.log(page_number);
                     scraper_list(page_number);
                 }, 3000
             );
@@ -36,45 +39,46 @@ function scraper_list(page_number) {
     })
 }
 
-scraper_list(1);
+function start() {
+    fs.readFile(urlfile, 'utf8', function (err, data) {
+        if (err) throw err;
+        url_data = JSON.parse(data);
+        scraper_houses(0)
+    })
+};
 
-function cb(err){
-    console.log('Test' + err)
+function scraper_houses(i) {
+    scrapeIt("http://list.am" + url_data.houses[i].url, {
+        houses: {
+            listItem: "#pcontent",
+            data: {
+                name: ".vih > h1",
+                type: ".c:nth-child(1) > .i",
+                rooms: ".c:nth-child(2) > .i",
+                square: ".c:nth-child(3) > .i",
+                price: ".price",
+                location: ".loc",
+                helping: ".footer"
+            }
+        }
+    }).then(function (page) {
+        datay.houses.push(...page.houses);
+        setTimeout(
+            () => {
+                console.log(i);
+                i = i + 1;
+                if (i < url_data.houses.length) {
+                    scraper_houses(i);
+                    if (i % 2 == 0) setTimeout(function(){
+                        console.log('Waiting for 5 minutes');
+                    }, 5000)
+                } else {
+                    jsonfile.writeFile(file, datay, { spaces: 2 });
+                }
+            }, 3000);
+    })
 }
 
 
-function scraper_houses(err) {
-    console.error(err);
-    fs.readFile(urlfile, 'utf8',
-        function (err, data) {
-            if (!err) {
-                console.log("Worked!");
-            }
-            var data = JSON.parse(data);
-            for (i = 0; i < data.houses.length-1; i++) {
-                console.log("http://list.am" + data.houses[i].url);
-                setTimeout(() => {
-                    scrapeIt("http://list.am" + data.houses[i].url, {
-                        houses: {
-                            listItem: "#pcontent",
-                            data: {
-                                name: ".vih > h1",
-                                type: ".c:nth-child(1) > .i",
-                                rooms: ".c:nth-child(2) > .i",
-                                square: ".c:nth-child(3) > .i",
-                                price: ".price",
-                                location: ".loc",
-                                helping: ".footer"
-                            }
-                        }
-                    }).then(function (page) {
-                        jsonfile.writeFile(file, page, { flag: "a" }, function (err) {
-                            if (!err) console.error(err);
-                            
-                        });
-                    })
-                }, 3000 * i);
-            }
-        }
-    )
-};
+start();
+//scraper_list();
